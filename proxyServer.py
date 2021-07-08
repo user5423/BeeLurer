c290d7dab9d505efbe8784dc92259ddf35ddb9e3
user5423 <davide.correia@outlook.com>
2021-07-08 20:15:32 +0100

    Added basic callback fucntionality on TCP data stream

--- proxyServer.py
+++ proxyServer.py
@@ -3,18 +3,23 @@ import threading
 import socketserver
 
 
-class proxyReqHandler(socketserver.BaseRequestHandler):
+##TODO: Accomodate large post requestse that are greater than 1024 bytes
 
+
+class proxyReqHandler(socketserver.BaseRequestHandler):
     def setup(self):
+        self.callback = lambda data: None
         ##We create a new connection with the http server on the local machine
         self.setupLocalProxyConnection()
 
+    def setTCPStreamCallback(self, callback):
+        self.callback = callback
+        return
 
     def setupLocalProxyConnection(self):
         self.proxyConn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.proxyConn.settimeout(0.5)
         self.proxyConn.connect(("127.0.0.1", 80))
-        print("Setup proxyConn")
 
 
     def handle(self):
@@ -27,7 +32,16 @@ class proxyReqHandler(socketserver.BaseRequestHandler):
         ## We then pass the data to the proxyConn that we setup
         retVal = self.proxyConn.sendall(self.data)
 
+        ##TODO: We need to recv in a way that we hold data in variable, and remove parts of it once we have found a finished request
         proxyData = self.proxyConn.recv(1024).strip()
+        
+        ##NOTE: This function will be called on streams of data segmented into blocks of up to 1024 bytes.
+        ##If the http request spans two blocks, or there is two http requests in a block, then the callback
+        ##won't be able to handle individual http requests
+        self.callback(proxyData)
+
+        ##TODO: There shouldn't be an issue about having multiple protocols used on the same socket connection, unless an adversary
+        ##is actively attempting to force it
 
         self.request.sendall(proxyData)
 
