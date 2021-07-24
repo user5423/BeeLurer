e288100a5e5dad2c35253415fbd8270c312cb0d2
user5423 <davide.correia@outlook.com>
2021-07-24 14:23:43 +0100

    Transferred comments over from deleted file over to baitConnector.py

--- baitConnector.py
+++ baitConnector.py
@@ -16,11 +16,50 @@ import copy
 from credentialGenerator import credentialGenerator
 from credentialGenerator import credentialGenerator as cg
 
+## Firstly, we need a docker container that will handle all the communication between the client and the server
+## This will include generating the unique credentials, updating a database, and alerting if credentials were used
+
+## The basic workflow would be as follows
+
+# 1. Client requests new credentials to be issued from server
+# 2. Client sends these credentials in plaintext through the Tor network
+# 3. Server receives the requests from the tor network, maps the credentials to the ip address receieved, with a date-time-stamp
+
+## NOTE: IP addresses tend to be recycled, and it could be possible (unlikely) that another exit node has that IP address some time in the future, so we don't want to flag it accidentally
+
+# 4. The server then in the future receieves a second request using those credentials (possibly from a different IP address), and maps it to the exit node
+# 5. The server would then provide this information to the correct authorities to deal with as feels
+
+
+## A better workflow is to eliminate the client from it
+# The client's only job would be to setup the server from their machine, then the machine would handle all work
+# This way an adversary wouldn't be able to intercept any requests on step 1, or would be able to generate their own credentials to cause mayhem through false reports
+
+## Simplified workflow:
+## NOTE: Assume that the server is setup
+
+# 1. The server generates new credentials
+# 2. The server then checks on its list of unused exit nodes where to route the request
+# 3. The request is routed to self through the tor network
+# 4. Once the request is receieved, the unique credentials are mapped to a ip address of an exit-node at a point in time
+# 5. If the credentials are then used in a future request, then send an alert to a pre-configured location (e.g. email, phone, webhook)
+
+
+## The potential architecture:
+
+# 1a. "Proxy container" -- this container will handle receiving the request, talking to the database, and passing the request to the main honeypot container
+# This can be considered a proxy server. However, we need to note that on file request
+
+# 1b. "Log-inspector container" -- alternatively, this container will look at the logs of the main honeypot service via a socket or a shared volume
+## The main honeyport server receieves the requests and logs them all, and every so often the log-inspector container will read it
+
+# The first solution provides seperation of the honeypot from the beelurer container, but port scanning might show other information
+# However, the second, would be a lot simpler to implemenent, but the first but be quite easy out of the box to configure
+
+
+
 ##TODO: Implement try and except argument with the custom errors used by stem
-
 ##We need a way to allow the user to show how to create the request
-
-
 class torSessionManager:
 	def __init__(self, torrcConfig):
 		self._setTorrcConfiguration(torrcConfig)
@@ -93,11 +132,14 @@ class torSessionManager:
 			print(e)
 			return None
 
+
 	def shutdownTorProcess(self):
 		self.tor_process.kill()
 
+
 	def shutdownTorController(self):
 		self.controller.close()
+
 
 
 class baitConnector(torSessionManager, credentialGenerator):
@@ -129,7 +171,6 @@ class baitConnector(torSessionManager, credentialGenerator):
 		##Then we find the variable in the template for url, headers, and body and replace them
 		requestTemplate = self.templateReplaceVariables(requestFormat)
 		##Return the requestTemplate to be used in the requestThroughTor methods
-		print(requestTemplate)
 		return requestTemplate
 
 
