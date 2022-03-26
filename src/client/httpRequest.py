8d7e7e591d584bd9d4f40dc402e8823de853e944
user5423 <davide.correia@outlook.com>
2022-03-26 00:49:09 +0000

    Added httpRequest class

--- src/client/httpRequest.py
+++ src/client/httpRequest.py
@@ -29,3 +29,34 @@ class httpBody:
     def setBody(self, body: Any, contentType: str):
         self._body = body
         self._content = contentType
+
+
+@dataclass
+class httpRequest:
+    url: str
+    method: str = field(default="GET")
+    headers: dict = field(default_factory=dict)
+    body: Any = field(default_factory=dict)
+    _contentType: str = field(default='application/json')
+    _supportedMethods: ClassVar[Set[str]] = {"GET", "POST", "PUT", "DELETE", "PATCH", "UPDATE", "HEAD", "CONNECT", "TRACE", "OPTIONS"}
+
+    def __post_init__(self):
+        self.url = self.createUrl(url)
+        self.headers = self.createHeaders(self.headers)
+        self.body = self.createBody(self.body, self._contentType)
+        self.headers.setHeaders({"content-type": self._contentType})
+    
+    def setHttpMethod(self, method: str) -> None:
+        if method not in httpRequest._supportedMethods:
+            raise UnsupportedHttpMethod
+        else:
+            self.method = method
+
+    def createUrl(self, url: str) -> None:
+        self.url = yarl.URL(url)
+
+    def createBody(self, data: Any, contentType: str) -> None:
+        self.body = httpBody().setBody(body, contentType)
+
+    def createHeaders(self, headers: Dict[str, str]) -> None:
+        self.headers = httpHeaders().setHeaders(headers)
