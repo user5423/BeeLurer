df1f76d1f3f26b1a32360c4a7fd62854840427c5
user5423 <davide.correia@outlook.com>
2022-03-30 12:24:34 +0100

    Cleanedup up and added typehinting to authenticationGenerator

--- src/client/authenticationGenerator.py
+++ src/client/authenticationGenerator.py
@@ -1,47 +1,52 @@
-from typing import Dict
 import random
 import secrets
-##TODO: We need to expand this from a simple username, password credential generation after PoC developed to a range of authentication methods
+from typing import Iterator, TextIO, NamedTuple
+
+## TODO: We need to expand this from a simple username, password credential generation
+## after PoC developed to a range of authentication methods
+
 class authenticationGenerator:
-    def __init__(self):
-        self.credentialsUsed = []
+	_usernameWordlistFile = "wordlists/jeanphorn-wordlist-usernames.txt"
+	_userpassFormat = NamedTuple("userpass", ["username", "password"])
 
-    def generateUserPass(self) -> Dict[str,str]:
-        userpass = {
-            "username": self.generateUsername(),
-            "password": self.generatePassword()
-        }
 
-        return userpass
+	def __init__(self) -> None:
+		self.credentialsUsed = []
+		self.usernameGenerator = None
 
-    def generateUsername(self):
-        try:
-            return next(self.usernameGenerator)
-        except Exception:
-            self.usernameGenerator = self.getUsernameFromFile()
-            print("started again")
-            return next(self.usernameGenerator)
 
-    def getUsernameFromFile(self, minLength=5, maxLength=13):
-        with open("wordlists/jeanphorn-wordlist-usernames.txt") as infile:
-            while True:
-                username = self.jumpForwardInFile(infile)
-                if minLength <= len(username) <= maxLength and username not in self.credentialsUsed:
-                    yield username                
+	def generateUserpass(self) -> "authenticationGenerator._userpass":
+		while True:
+			userpass = authenticationGenerator._userpassFormat(self.generateUsername(), self.generatePassword())
+			if userpass not in self.credentialsUsed:
+				self.credentialsUsed.append(userpass)
+				break
 
-    def jumpForwardInFile(self, fd, lines=random.randint(170, 250)):
-        for i in range(lines):
-            username = fd.readline().strip("\n")
-        return username
+		return userpass
 
-    def generatePassword(self, minLength=8, maxLength=12):
-        return secrets.token_urlsafe(random.randint(minLength, maxLength))
-            
-    def generateCredentials(self):
-        while True:
-            userpassCreds = self.generateUsername(), self.generatePassword()
-            if userpassCreds not in self.credentialsUsed:
-                self.credentialsUsed.append(userpassCreds)
-                break
 
-        return userpassCreds
+	def _generateUsername(self) -> str:
+		try:
+			return next(self.usernameGenerator)
+		except StopIteration:
+			self.usernameGenerator = self._getUsernameFromFile()
+			return next(self.usernameGenerator)
+
+
+	def _getUsernameFromFile(self, minLength: int = 5, maxLength: int = 13) -> Iterator[str]:
+		with open(authenticationGenerator._usernameWordlistFile, "r") as infile:
+			while True:
+				username = self._jumpForwardInFile(infile)
+				if minLength <= len(username) <= maxLength and username not in self.credentialsUsed:
+					yield username                
+
+
+	def _jumpForwardInFile(self, fd: TextIO(), lines: int = random.randint(170, 250)) -> str:
+		for _ in range(lines):
+			username = fd.readline().strip("\n")
+		return username
+
+
+	def _generatePassword(self, minLength: int = 8, maxLength: int = 12) -> str:
+		return secrets.token_urlsafe(random.randint(minLength, maxLength))
+			
