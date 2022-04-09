ee06473ae65dfa20224387b7c975c8a3ffa93bae
user5423 <davide.correia@outlook.com>
2022-04-09 12:52:35 +0100

    Added boilerplate tests for  class

--- tests/unit/unit.httpRequest.py
+++ tests/unit/unit.httpRequest.py
@@ -6,9 +6,9 @@ from httpRequest import httpAuthGen, httpHeaders, httpBody, \
 						httpQuery, url, httpRequest, httpRequestBuilder
 
 
-
 class test_httpAuthGen:
 	...
+
 
 class test_httpHeaders:
 	def test_setHeader_noHeaders(self):
@@ -44,6 +44,7 @@ class test_httpHeaders:
 	def test_generateHeaderList_someHeaders(self):
 		...
 
+
 class test_httpBody:
 	def test_setBody_settingNew(self):
 		...
@@ -68,15 +69,70 @@ class test_httpQuery:
 	def test_removeQuery_existingQuery(self):
 		...
 
+
 class test_url:
-	...
+	def test_setUrl_settingNew(self):
+		...
 
+	def test_setUrl_overwriting(self):
+		...
 
-
+	def test_setUrl_invalidUrlFormat(self):
+		...
 
 
 class test_httpRequest:
-	...
+	## NOTE: HTTP Url setting
+	def test_init_createUrl_settingNew(self):
+		...
+
+	def test_init_createUrl_invalidUrlFormat(self):
+		...
+
+	
+	## NOTE: HTTP Method setting
+	def test_init_setHttpMethod_unsupportedMethod(self):
+		...
+
+	def test_init_setHttpMethod_invalidMethod(self):
+		...
+
+	def test_init_setHttpMethod_supportedMethod(self):
+		...
+
+
+	## NOTE: HTTP Header setting
+	def test_init_createHeaders_settingNew(self):
+		...
+
+
+	## NOTE: HTTP Body setting
+	def test_init_createBody_settingNew(self):
+		...
+
+	def test_init_createBody_mismatchingType(self):
+		...
+
+
+	## NOTE: HTTP Query setting
+	def test_init_createQuery_settingNew(self):
+		...
+
+
+	## NOTE: Auth setting
+	def test_init_setAuth_settingNew(self):
+		...
+
+	def test_init_setAuth_unsupportedType(self):
+		...
+
+	def test_init_setAuth_invalidType(self):
+		...
+
+	## NOTE: Testing redundancy mismatches
+	def test_init_contentTypeMismatch(self):
+		...
+
 
 class test_httpRequestBuilder:
 	...
