d7552055c6ae6f14d44b20b1c93f38ac320101fe
user5423 <davide.correia@outlook.com>
2022-04-09 12:41:22 +0100

    Added boilerplate tests for  class

---
+++ tests/unit/unit.httpRequest.py
@@ -0,0 +1,64 @@
+import sys
+import pytest
+sys.path.insert(0, "src/client/")
+
+from httpRequest import httpAuthGen, httpHeaders, httpBody, \
+						httpQuery, url, httpRequest, httpRequestBuilder
+
+
+
+class test_httpAuthGen:
+	...
+
+class test_httpHeaders:
+	def test_setHeader_noHeaders(self):
+		...
+
+	def test_setHeader_someHeaders(self):
+		...
+
+	def test_setHeader_existingHeader(self):
+		...
+
+	def test_setHeaders_noHeaders(self):
+		...
+
+	def test_setHeaders_someHeaders(self):
+		...
+
+	def test_setHeaders_existingHeaders(self):
+		...
+
+	def test_removeHeader_noHeaders(self):
+		...
+
+	def test_removeHeader_someHeaders(self):
+		...
+
+	def test_removeHeader_existingHeader(self):
+		...
+
+	def test_generateHeaderList_noHeaders(self):
+		...
+
+	def test_generateHeaderList_someHeaders(self):
+		...
+
+class test_httpBody:
+	...
+
+class test_httpQuery:
+	...
+
+class test_url:
+	...
+
+
+
+
+
+class test_httpRequest:
+	...
+
+class test_httpRequestBuilder:
+	...
