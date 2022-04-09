b8eb5605229af1e6c25511ecb4de5f1fbbbb2235
user5423 <davide.correia@outlook.com>
2022-04-09 17:33:44 +0100

    Added basic boilerplate tests for baitConnector class

---
+++ tests/unit/unit.baitConnector.py
@@ -0,0 +1,50 @@
+import sys
+import pytest
+sys.path.insert(0, "src/client/")
+
+from baitConnector import baitConnector
+
+
+class test_baitConnector:
+	## TODO: Expand these tests from torSessionManager init tests
+	def test_init_baitConnector_invalidTorrcConfig(self):
+		...
+
+	def test_init_baitConnector_validTorrcConfig(self):
+		...
+
+
+
+	## TODO: Currently we are storing the exit node in memory instead of a
+	## persistent database
+	def test_logExitNodeConnection_noConnection(self):
+		...
+
+	def test_logExitNodeConnection_singleConnection(self):
+		...
+
+	def test_logExitNodeConnection_multipleConnections(self):
+		...
+
+	def test_logExitNodeConnection_previouslyConnected(self):
+		...
+
+
+
+	## TODO: These will be modified to abc.abstractmethods later so we want
+	## to test for that
+	def test_performBaitOperation(self):
+		...
+
+	def test_createRequestObject(self):
+		...
+
+
+
+	def test_testExitNode(self):
+		...
+
+	def test_run(self):
+		...
+
+	
