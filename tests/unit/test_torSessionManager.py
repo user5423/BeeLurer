927ba6e2d4ce0265744567320305b1b9fe137cf7
user5423 <davide.correia@outlook.com>
2022-04-09 20:29:58 +0100

    Added additional boilerplate tests for 'torSessionManager' class

--- tests/unit/test_torSessionManager.py
+++ tests/unit/test_torSessionManager.py
@@ -7,84 +7,91 @@ from torSessionManager import torSessionManager
 
 class Test_TorSessionManager:
 
-    ## These unittests should test: 
-    ## 1. torSessionManager._setTorrcConfiguration()
-    ## 2. torSessionManager._initiateTorProcess()
+	## These unittests should test: 
+	## 1. torSessionManager._setTorrcConfiguration()
+	## 2. torSessionManager._initiateTorProcess()
 
-    def test_init_Default(self):
-        ...
+	def test_init_Default(self):
+		...
 
-    def test_init_EmptyDict(self):
-        ...
+	def test_init_EmptyDict(self):
+		...
 
-    def test_init_AbsentConfigKeys(self):
-        ...
+	def test_init_AbsentConfigKeys(self):
+		...
 
-    def test_init_IncorrectValueConfigKey(self):
-        ...
+	def test_init_IncorrectValueConfigKey(self):
+		...
 
-    def test_init_IncorrectValueConfigKeys(self):
-        ...
+	def test_init_IncorrectValueConfigKeys(self):
+		...
 
-    def test_init_NonexistentConfigKeys(self):
-        ...
+	def test_init_NonexistentConfigKeys(self):
+		...
 
-    def test_init_IncorrectFormatConfigValue(self):
-        ...
+	def test_init_IncorrectFormatConfigValue(self):
+		...
 
-    def test_init_IncorrectFormatConfigValues(self):
-        ...
+	def test_init_IncorrectFormatConfigValues(self):
+		...
 
-    def test_init_IncorrectValueConfigValues(self):
-        ...
+	def test_init_IncorrectValueConfigValues(self):
+		...
 
-    def test_init_IncorrectValuesConfigValues(self):
-        ...
-
-
-    ## These unittests should test
-    ## 1. torSessionManager._initiateTorProccess()
-
-    def test_init_ProcessAlreadyListeningOnPort(self):
-        ...
-
-    def test_init_TorNotInstalled(self):
-        ...
-
-    def test_init_TorProcessFailedToStartup(self):
-        ...
-
-    def test_init_torSessionManagerObjectKilled(self):
-        ...
-
-    def test_init_torSessionManagerProcessKilled(self):
-        ...
+	def test_init_IncorrectValuesConfigValues(self):
+		...
 
 
-    ## These unittests should test:
-    ## 1. torSessionManager._initiateTorController(self):
+	## These unittests should test
+	## 1. torSessionManager._initiateTorProccess()
 
-    def test_init_TorControlPortAlreadyListened(self):
-        ...
+	def test_init_ProcessAlreadyListeningOnPort(self):
+		...
 
-    def test_init_TorControlPortNotListened(self):
-        ...
+	def test_init_TorNotInstalled(self):
+		...
 
-    def test_init_TorControlFailedToAuthenticate(self):
-        ...
+	def test_init_TorProcessFailedToStartup(self):
+		...
 
+	def test_init_torSessionManagerObjectKilled(self):
+		...
 
-    ## These unittests should test all functions called by torSessionManager.__init__():
-    ## 1. torSessionManager._setTorrcConfiguration()
-    ## 2. torSessionManager._initiateTorProcess()
-    ## 3. torSessionManager._initiateTorController(self):
-
-    def test_init_defaultSuccess(self):
-        ...
-
-    def test_init_customSuccess(self):
-        ...
+	def test_init_torSessionManagerProcessKilled(self):
+		...
 
 
+	## These unittests should test:
+	## 1. torSessionManager._initiateTorController(self):
 
+	def test_init_TorControlPortAlreadyListened(self):
+		...
+
+	def test_init_TorControlPortNotListened(self):
+		...
+
+	def test_init_TorControlFailedToAuthenticate(self):
+		...
+
+
+	## These unittests should test all functions called by torSessionManager.__init__():
+	## 1. torSessionManager._setTorrcConfiguration()
+	## 2. torSessionManager._initiateTorProcess()
+	## 3. torSessionManager._initiateTorController(self):
+
+	def test_init_defaultSuccess(self):
+		...
+
+	def test_init_customSuccess(self):
+		...
+
+
+	def test_retrieveExitNodes(self):
+		...
+
+	def test_changeExitNode(self):
+		...
+
+	def test_shutdown(self):
+		...
 
