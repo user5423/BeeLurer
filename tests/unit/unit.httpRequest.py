4eb4597f8a86c896bfa01735e16eea4b649226ab
user5423 <davide.correia@outlook.com>
2022-04-09 12:52:35 +0100

    Added boilerplate getter, redundancyChecks, and additional init tests for 'httpRequest' class

--- tests/unit/unit.httpRequest.py
+++ tests/unit/unit.httpRequest.py
@@ -6,9 +6,9 @@ from httpRequest import httpAuthGen, httpHeaders, httpBody, \
 						httpQuery, url, httpRequest, httpRequestBuilder
 
 
-
 class test_httpAuthGen:
 	...
+
 
 class test_httpHeaders:
 	def test_setHeader_noHeaders(self):
@@ -44,6 +44,7 @@ class test_httpHeaders:
 	def test_generateHeaderList_someHeaders(self):
 		...
 
+
 class test_httpBody:
 	def test_setBody_settingNew(self):
 		...
@@ -68,15 +69,104 @@ class test_httpQuery:
 	def test_removeQuery_existingQuery(self):
 		...
 
+
 class test_url:
-	...
+	def test_setUrl_settingNew(self):
+		...
 
+	def test_setUrl_overwriting(self):
+		...
 
-
+	def test_setUrl_invalidUrlFormat(self):
+		...
 
 
 class test_httpRequest:
-	...
+	## NOTE: Object initialization checking
+	def test_init_createUrl_settingNew(self):
+		...
+
+	def test_init_createUrl_invalidUrlFormat(self):
+		...
+
+	def test_init_setHttpMethod_unsupportedMethod(self):
+		...
+
+	def test_init_setHttpMethod_invalidMethod(self):
+		...
+
+	def test_init_setHttpMethod_supportedMethod(self):
+		...
+
+	def test_init_createHeaders_settingNew(self):
+		...
+
+	def test_init_createBody_settingNew(self):
+		...
+
+	def test_init_createBody_mismatchingType(self):
+		...
+
+	def test_init_createQuery_settingNew(self):
+		...
+
+	def test_init_authSetting_settingNew(self):
+		...
+
+	def test_init_authSetting_unsupportedType(self):
+		...
+
+	def test_init_authSetting_invalidType(self):
+		...
+
+
+
+	## NOTE: Object getter methods
+	def test_getMethod(self):
+		...
+
+	def test_getUrl_noQueries(self):
+		...
+	
+	def test_getUrl_singleKeyValueQuery(self):
+		...
+
+	def test_getUrl_manyKeyValueQueries(self):
+		...
+
+	def test_getUrl_singleKeyOnlyQuery(self):
+		...
+
+	def test_getUrl_manyKeyOnlyQuery(self):
+		...
+
+	def test_getUrl_mixedKeyOnlyAndKeyValueQueries(self):
+		...
+
+	def test_getBody(self):
+		...
+
+	def test_getHeaders_noHeaders(self):
+		...
+
+	def test_getHeaders_singleHeader(self):
+		...
+
+	def test_getHeaders_manyHeaders(self):
+		...
+
+	def test_getQueries(self):
+		...
+
+
+
+	## NOTE: Testing redundancy mismatches
+	def test_init_contentTypeMismatch(self):
+		...
+
+	def test_queriesAndUrlQueryStringMistmatch(self):
+		...
+
 
 class test_httpRequestBuilder:
 	...
